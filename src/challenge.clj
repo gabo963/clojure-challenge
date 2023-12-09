40048e8e2a8e53de22ff42f254d80cc03ea49284
Gabriel <git@github.com>
2023-12-08 19:17:02 -0500

    done without last

--- src/challenge.clj
+++ src/challenge.clj
@@ -1,5 +1,7 @@
 (ns challenge)
 
+(def invoice (clojure.edn/read-string (slurp "invoice.edn")))
+
 (defn invoice-filter-Last
   [invoice]
   (->> invoice
@@ -7,9 +9,8 @@
        ))
 
 (defn check-tax
-  [tax-set rate percentage]
-  (filter #(and (= rate (:tax/category %)) (= percentage (:tax/rate %)) ) tax-set)
-  )
+  [tax-set category percentage keys]
+  (not-empty (filter #(and (= category ((first keys) %)) (= percentage ((second keys) %)) ) tax-set)) )
 
 (defn xor
   [cond1 cond2]
@@ -20,8 +21,10 @@
 
 (defn invoice-filter
   [invoice]
-  (reduce (fn [item]
-            (if (xor (check-tax (:taxable/taxes items) 19 :iva) )  ))
+  (reduce (fn [items item]
+            (if (xor (check-tax (:taxable/taxes item) :iva 19 [:tax/category :tax/rate]) (check-tax (:retentionable/retentions item) :ret_fuente 1 [:retention/category :retention/rate]) )
+              (conj items item)
+              items))
           []
           (:invoice/items invoice)
           ))
